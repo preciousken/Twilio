fca57625c395d26bf1dc730c2deb70ab994d99ac
preciousken <pkehinde27@gmail.com>
2023-04-02 03:59:24 +0100

    twilio messaging

--- app.js
+++ app.js
@@ -1,1 +1,77 @@
-///
+const express = require('express')
+const cors = require('cors')
+const { sendMessage } = require('./sms')
+const app = express()
+require('dotenv').config()
+
+const recipientName = process.env.MESSAGE_RECEIPIENT_NAME
+
+app.use(express.json())
+app.use(cors())
+
+app.post('/send',async(req,res)=>{
+
+     // getting the form data
+     body = req.body
+
+    //  Handling error if no message was provided
+     if(!body.message){
+         error='MESSAGE_ERROR',
+         res.status(401).json({
+             error:error,
+             message:`You haven't provided a message`
+         })
+         return;
+     }
+
+
+     //  Handling error if no sender was provided
+     if(!body.sender){
+        error='SENDER_ERROR',
+        res.status(401).json({
+            error:error,
+            message:`You haven't provided a message`
+        })
+        return;
+    }
+
+    //  Handling error if sender is not a Number was provided
+    if((typeof(body.message)) === Number){
+        error='SENDER_ERROR',
+        res.status(401).json({
+            error:error,
+            message:`Sender must a name`
+        })
+        return;
+    }
+
+
+    try {
+
+         // send a message to precious
+    sendMessage({
+        message:body.message,
+        sender:body.sender
+    })
+    
+    res.status(400).json({
+        status:true,
+        message:`message has been delivered to ${recipientName}`
+    })
+      return;          
+    } catch (error) {
+        console.log(error)
+        error='UNKNOWN_ERROR',
+        res.status(500).json({
+            error:error,
+            message:`You've got some errors`
+        })
+        return;
+    }
+})
+
+port = process.env.PORT
+
+app.listen(port,()=>{
+    console.log(`App listening on port ${port}`);
+})
---
+++ sms.js
@@ -0,0 +1,21 @@
+// Download the helper library from https://www.twilio.com/docs/node/install
+// Find your Account SID and Auth Token at twilio.com/console
+// and set the environment variables. See http://twil.io/secure
+const sendMessage =async ({message,sender})=>{
+    require('dotenv').config()
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const twilioNumber = process.env.TWILIO_NUMBER;
+const myNumber = process.env.MY_NUMBER;
+const client = require('twilio')(accountSid, authToken);
+
+client.messages
+  .create({
+     body: `${message} --From ${sender}`,
+     from: twilioNumber,
+     to: myNumber
+   })
+
+}
+
+module.exports={sendMessage}
